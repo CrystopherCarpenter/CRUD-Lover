88b6fd59a9aa9f31b3f1076606b6aa5e2bc1a825
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-19 12:21:18 -0300

    first commit

---
+++ src/main/java/com/crudlover/api/model/Car.java
@@ -0,0 +1,46 @@
+package com.crudlover.api.model;
+
+import java.util.Date;
+
+import com.crudlover.api.dto.CarsDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String modelo;
+
+    @Column(length = 20, nullable = false)
+    private String fabricante;
+
+    @Column(length = 20, nullable = false)
+    private Date dataFabricacao;
+
+    @Column(length = 20, nullable = false)
+    private int valor;
+
+    @Column(length = 20, nullable = false)
+    private int anoModelo;
+
+    public Car(CarsDTO data) {
+        this.modelo = data.modelo();
+        this.fabricante = data.fabricante();
+        this.dataFabricacao = data.dataFabricacao();
+        this.valor = data.valor();
+        this.anoModelo = data.anoModelo();
+    }
+}
