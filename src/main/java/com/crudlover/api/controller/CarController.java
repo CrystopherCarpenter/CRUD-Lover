131a71b36abd375e1b99a71353287941a3ee0729
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-23 12:45:20 -0300

    add service layer, pagination  and http status response

---
+++ src/main/java/com/crudlover/api/controller/CarController.java
@@ -0,0 +1,66 @@
+package com.crudlover.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crudlover.api.dto.CarDTO;
+import com.crudlover.api.model.Car;
+import com.crudlover.api.service.CarService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    private CarService service;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void create(@RequestBody @Valid CarDTO req) {
+        service.save(new Car(req));
+    }
+
+    @GetMapping
+    public List<Car> listAll(@PageableDefault(page = 0) Pageable page) {
+        return service.findAll(page.getPageNumber());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findById(@PathVariable long id) {
+        var car = service.findById(id);
+
+        return (car != null) ? ResponseEntity.ok().body(car)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable long id, @RequestBody @Valid CarDTO req) {
+        service.update(id, req);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id) {
+        service.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/crudlover/api/service/CarService.java
@@ -0,0 +1,49 @@
+package com.crudlover.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.crudlover.api.dto.CarDTO;
+import com.crudlover.api.model.Car;
+import com.crudlover.api.repository.CarRepository;
+
+@Service
+public class CarService {
+
+    @Autowired
+    private CarRepository repository;
+
+    public void save(Car req) {
+        repository.save(req);
+    }
+
+    public List<Car> findAll(int page) {
+        PageRequest pagination = PageRequest.of(page, 5);
+
+        return repository.findAll(pagination).getContent();
+    }
+
+    public Car findById(long id) {
+        return repository.findById(id).map(car -> car).orElse(null);
+    }
+
+    public void update(long id, CarDTO req) {
+        repository.findById(id).map(car -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
+    }
+
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
+
+}
